f108ce67c8d761b2ad04c4f1c1bd917b733e83ca
vbrodi23-lgtm <vbrodi23@gmail.com>
2026-08-09 14:48:43 +1000

    Build self-generated GSN cache bridge

---
+++ src/windows_solver/data/julia/GeneralizedSasakiNakamura.jl/src/Homogeneous/Kerr.jl
@@ -0,0 +1,26 @@
+module Kerr
+
+export Delta, r_plus, r_minus, omega_horizon, K
+
+function Delta(a, r)
+    r^2 - 2*r + a^2
+end
+
+function r_plus(a)
+    1 + sqrt(1 - a^2)
+end
+
+function r_minus(a)
+    1 - sqrt(1 - a^2)
+end
+
+function omega_horizon(a)
+    rp = r_plus(a)
+    return a/(2*1*rp)
+end
+
+function K(m::Int, a, omega, r)
+    (r^2 + a^2)*omega - m*a
+end
+
+end
